61f89df2cff7d171fe56304f00424c845a9d9902
tomokazu kozuma <tomokazu.kozuma@gmail.com>
2021-10-02 17:00:20 +0900

    Split the create/decode command into subcommands

--- cmd/create.go
+++ cmd/create.go
@@ -16,12 +16,19 @@ limitations under the License.
 package cmd
 
 import (
+	"crypto/ed25519"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
+	"github.com/tomokazukozuma/polkadot-cli/lib/encode"
 )
 
-// addressCmd represents the address command
-var addressCmd = &cobra.Command{
-	Use:   "address",
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -30,20 +37,40 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		publicKey, privateKey, err := ed25519.GenerateKey(nil)
+		if err != nil {
+			log.Fatalf("Failed GenerateKey", err)
+		}
 
+		ss58Prefix, err := cmd.Flags().GetInt8("ss58Prefix")
+		if err != nil {
+			log.Fatalf("Failed Get ss58Prefix: %s", err.Error())
+		}
+		address := encode.EncodeAddress(publicKey, ss58Prefix)
+
+		data := [][]string{
+			{"PrivateKey", fmt.Sprintf("%x", privateKey)},
+			{"PublicKey", fmt.Sprintf("%x", publicKey)},
+			{"Address", fmt.Sprintf("%s", address)},
+		}
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetBorder(true)
+		table.AppendBulk(data)
+		table.Render()
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(addressCmd)
+	addressCmd.AddCommand(createCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// addressCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// addressCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	createCmd.Flags().Int8("ss58Prefix", 0, "SS58Prefix 0: Polkadot, 2: Kusama, 42: Westend")
 }
--- cmd/decode.go
+++ cmd/decode.go
@@ -16,12 +16,19 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/tomokazukozuma/polkadot-cli/lib/encode"
+
 	"github.com/spf13/cobra"
 )
 
-// addressCmd represents the address command
-var addressCmd = &cobra.Command{
-	Use:   "address",
+// decodeCmd represents the decode command
+var decodeCmd = &cobra.Command{
+	Use:   "decode",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -30,20 +37,37 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		address, err := cmd.Flags().GetString("address")
+		if err != nil {
+			log.Fatalf("Failed Get create: %s", err.Error())
+		}
+		publicKey, ss58Prefix, err := encode.DecodeAddress(address)
+		if err != nil {
+			log.Fatalf("Failed Get create: %s", err.Error())
+		}
+		data := [][]string{
+			{"Address", fmt.Sprintf("%s", address)},
+			{"PublicKey", fmt.Sprintf("%x", publicKey)},
+			{"ss58Prefix", fmt.Sprintf("%d", ss58Prefix)},
+		}
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetBorder(true)
+		table.AppendBulk(data)
+		table.Render()
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(addressCmd)
+	addressCmd.AddCommand(decodeCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// addressCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// decodeCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// addressCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// decodeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	decodeCmd.Flags().String("address", "", "address")
 }
